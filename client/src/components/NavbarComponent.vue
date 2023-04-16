28da0390051b8d389cc749db24e9835593277fb4
EliasSchaut <eschaut@outlook.de>
2023-04-16 21:48:00 +0200

    client: change names

--- client/src/components/NavbarComponent.vue
+++ client/src/components/NavbarComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
     <div class="container-fluid">
-      <router-link class="navbar-brand" to="/">Movie Monday Manager</router-link>
+      <router-link class="navbar-brand" to="/">Winvite</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
               data-bs-target="#navbarSupportedContent"
               aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
